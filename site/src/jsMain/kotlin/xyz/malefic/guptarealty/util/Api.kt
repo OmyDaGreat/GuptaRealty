634ac6b47f6727cbd9d81185b377972d1814ceb6
Om Gupta <ogupta4242@gmail.com>
2026-07-05 21:51:18 -0700

    fix(api): remove deprecated function calls new network api from kobweb is better but like at what cost yk? there rly should be a better kotlin js network thing oh wait ktor lol i rly should learn that one and transition to it and/or make a wrapper for it so it's more functional like http4k

--- site/src/jsMain/kotlin/xyz/malefic/guptarealty/util/Api.kt
+++ site/src/jsMain/kotlin/xyz/malefic/guptarealty/util/Api.kt
@@ -1,15 +1,17 @@
 package xyz.malefic.guptarealty.util
 
 import com.varabyte.kobweb.browser.api
+import com.varabyte.kobweb.browser.http.bodyAsBytes
+import com.varabyte.kobweb.browser.http.bodyOf
 import kotlinx.browser.window
 import xyz.malefic.guptarealty.model.json
-import kotlin.collections.emptyList
 
 suspend inline fun <reified T> getApi(
     url: String,
     token: String? = null,
 ) = window.api
-    .getBytes(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
+    .get(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
+    .bodyAsBytes()
     .decodeToString()
     .let { json.decodeFromString<T>(it) }
 
@@ -17,7 +19,8 @@ suspend inline fun <reified E> getApiList(
     url: String,
     token: String? = null,
 ) = window.api
-    .getBytes(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
+    .get(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
+    .bodyAsBytes()
     .decodeToString()
     .let {
         try {
@@ -31,29 +34,31 @@ suspend inline fun <reified E> getApiList(
 suspend fun postApi(
     url: String,
     token: String? = null,
-) = window.api.postBytes(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
+) = window.api.post(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
 
 suspend inline fun <reified T> postApi(
     url: String,
     body: T,
     token: String? = null,
-) = window.api.postBytes(
-    url,
-    body = json.encodeToString(body).encodeToByteArray(),
-    headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap(),
-)
+) = window.api
+    .post(
+        url,
+        bodyOf(json.encodeToString(body), "application/json"),
+        headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap(),
+    )
 
 suspend inline fun <reified T> putApi(
     url: String,
     body: T,
     token: String? = null,
-) = window.api.putBytes(
-    url,
-    body = json.encodeToString(body).encodeToByteArray(),
-    headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap(),
-)
+) = window.api
+    .put(
+        url,
+        bodyOf(json.encodeToString(body), "application/json"),
+        headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap(),
+    )
 
 suspend fun deleteApi(
     url: String,
     token: String? = null,
-) = window.api.deleteBytes(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
+) = window.api.delete(url, headers = token?.let { mapOf("Authorization" to "Bearer $it") } ?: emptyMap())
